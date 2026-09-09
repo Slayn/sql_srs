309e8030c71bc8d902a2048f7afc764545edec6e
Slayn <neoslayn@gmail.com>
2026-09-09 08:35:00 +0200

    feat: organisation de l'UI
    * add selectbox
    
    * feat: organisation de l'UI
    
    * feat: organisation de l'UI
    
    * feat: organisation de l'UI
    
    ---------
    
    Co-authored-by: LERDA Nicolas <nicolas.lerda@enovacom.com>

--- app.py
+++ app.py
@@ -1,23 +1,58 @@
+#import io
+
 import duckdb
 import pandas as pd
 import streamlit as st
 
-st.write("SQL - SRS")
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Howl"])
-
+# Gestion des données
 data = {
     'store_id': ["Armentieres", "Armentieres", "Armentieres", "Armentieres", "Lille", "Lille", "Lille", "Lille", "Douai", "Douai", "Douai", "Douai"],
     'product_name': ['redbull', 'chips', 'wine', 'redbull', 'redbull', 'chips', 'wine', 'icecream', 'redbull', 'chips', 'wine', 'icecream'],
     'amount': [45, 60, 60, 45, 100, 140, 190, 170, 55, 70, 20, 45]
 }
-df = pd.DataFrame(data)
+data = pd.DataFrame(data)
 
+answer = """
+SELECT * FROM data
+WHERE product_name = 'redbull'
+"""
+solution = duckdb.sql(answer).df()
+
+
+# Titre du programme
+st.write("SQL - SRS")
+
+
+# Menu gauche
+with st.sidebar:
+    option = st.selectbox(
+        "Choix du sujet à réviser :",
+        ("Joins", "GroupBy", "Windows Function"),
+        index=None,
+        placeholder="Selection du sujet"
+    )
+
+    st.write('Sujet choisi : ', option)
+
+
+# Header / toujours affiché
+input_sql = st.text_area(label="Entrez votre requête :", key="user_input")
+
+if input_sql != '':
+    resutlat_sql = duckdb.sql(input_sql).df()
+    st.dataframe(resutlat_sql)
+
+
+# Tab list
+tab1, tab2 = st.tabs(["Tables", "Solution"])
+
+# Tabs content
 with tab1:
-    st.write('Données')
-    st.write(df)
-    input_sql = st.text_area(label="Entrez votre requête :")
-    
-    if input_sql != '':
-        resutlat_sql = duckdb.sql(input_sql).df()
-        st.write(resutlat_sql)
+    st.write('Table : data')
+    st.dataframe(data)
+    st.write('Résultat attendu :')
+    st.dataframe(solution)
+
+with tab2:
+    st.write(answer)
